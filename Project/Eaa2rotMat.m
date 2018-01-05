ba8eae9e0e8ecf55fb7d7a5ab341002b42c23f22
Oriol de Dios <orioldedios@gmail.com>
2018-01-06 00:02:13 +0100

    rotation matrix calculated + cube rotate

---
+++ Project/Eaa2rotMat.m
@@ -0,0 +1,14 @@
+function [matrix] = Eaa2rotMat(axis,angle)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes her
+    %if sqrt(axis(3,1)^2+axis(2,1)^2+axis(1,1)^2)==1
+    axis = axis/norm(axis);
+    
+    I=eye(3);
+
+        u=[0 -axis(3,1) axis(2,1);axis(3,1) 0 -axis(1,1);-axis(2,1) axis(1,1) 0];
+
+        matrix=I*cos(angle)+(1-cos(angle))*(axis*axis')+u*sin(angle);
+    %end
+end
+
---
+++ Project/GetRotVec.m
@@ -0,0 +1,16 @@
+function [ vec ] = GetRotVec(radius,x,y)
+%GET Summary of this function goes here
+%   Detailed explanation goes here
+    vec=0;
+    
+    if((x.^2+y.^2) < 0.5*radius.^2)        
+        z = sqrt(radius.^2 - x.^2 - y.^2);
+        vec = [x, y, z]';
+        
+    else 
+        z = (radius.^2)/2*sqrt(x.^2 + y.^2);
+        vec = [x, y, z]'/sqrt(x.^2 + y.^2+Z.^2);
+    end
+
+end
+
